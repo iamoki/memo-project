ca01ddf79e690dfdc819129104647d22fd1a8425
iamoki <pureaoh@gmail.com>
2022-07-24 17:35:59 +0900

    ✅ 기본컴포넌트 구현

--- src/App.js
+++ src/App.js
@@ -1,21 +1,27 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
+import MemoContainer from './components/MemoContainer';
+import SideBar from './components/SideBar';
 
 function App() {
+  const [memos, setMemos] = useState([
+    {
+      title: 'Memo 1',
+      content: 'This is Memo 1',
+      createdAt: 1658651088625,
+      updatedAt: 1658651088625,
+    },
+    {
+      title: 'Memo 2',
+      content: 'This is Memo 2',
+      createdAt: 1658651109922,
+      updatedAt: 1658651109922,
+    },
+  ]);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>test</p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <SideBar memos={memos} />
+      <MemoContainer />
     </div>
   );
 }
